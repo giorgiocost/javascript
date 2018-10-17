473c1d4898ac9de8061f498ffbc86ace97254587
giorgiocost <gegiorgio1991@gmail.com>
2018-10-17 11:14:28 -0300

    Evento e Mouse OnClick

---
+++ basico/js/tiposDeEventos.js
@@ -0,0 +1,16 @@
+function mouseDown() {
+    console.log('apertou botão');
+}
+
+function mouseUp() {
+    console.log('soltou botão');
+}
+
+function mouseOver() {
+    console.log('passou por cima do botão');
+}
+
+
+function click() {
+    console.log('clicou');
+}
