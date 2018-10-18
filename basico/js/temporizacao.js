12b5534c3ffaa57a1b6f4774992bb1b29af374d6
giorgiocost <gegiorgio1991@gmail.com>
2018-10-18 15:22:52 -0300

    Temporizador

---
+++ basico/js/temporizacao.js
@@ -0,0 +1,20 @@
+// setInterval(function () {
+//     console.log('primeiro teste');
+// },1000);
+
+
+
+function segundoteste() {
+    console.log('segundo teste');
+}
+
+
+//loop
+ var teste = setInterval(segundoteste,1000);
+
+
+ // stop setInterval
+ clearInterval(teste);
+
+// executa uma vez só
+// setTimeout(segundoteste,1000);
